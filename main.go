5e8d48602182b227913ecd30b016a01bcf9d0524
Noah Snelson <noah.snelson@protonmail.com>
2020-05-24 15:24:51 -0700

    Finish challenge 3, add functions in challenge 2 to package scope

--- main.go
+++ main.go
@@ -7,9 +7,12 @@ import (
 
 func main() {
 	base64 := set1.Challenge1("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
-	fmt.Println("solution to challenge 1: ", base64, "\n")
+	fmt.Println("solution to challenge 1:", base64, "\n")
 
 	chal2input1 := "1c0111001f010100061a024b53535009181c"
 	chal2input2 := "686974207468652062756c6c277320657965"
-	fmt.Println("solution to challenge 2:", set1.Challenge2(chal2input1, chal2input2))
+	fmt.Println("solution to challenge 2:", set1.Challenge2(chal2input1, chal2input2), "\n")
+
+	chal3sol := set1.Challenge3("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+	fmt.Println("solution to challenge 3:", chal3sol)
 }
--- set1/challenge2.go
+++ set1/challenge2.go
@@ -6,22 +6,26 @@ import (
 )
 
 func Challenge2(input1 string, input2 string) string {
-	rawBytes1 := hexStringToBytes(input1)
-	rawBytes2 := hexStringToBytes(input2)
+	rawBytes1 := HexStringToBytes(input1)
+	rawBytes2 := HexStringToBytes(input2)
 	xorBytes := make([]byte, len(rawBytes2))
 	for idx, _ := range rawBytes1 {
 		xorBytes[idx] = rawBytes1[idx] ^ rawBytes2[idx]
 	}
-	output := make([]byte, hex.EncodedLen(len(xorBytes)))
-	hex.Encode(output, xorBytes)
-	return string(output)
+	return BytesToHexString(xorBytes)
 }
 
-func hexStringToBytes(input string) []byte {
+func HexStringToBytes(input string) []byte {
 	rawBytes := make([]byte, hex.DecodedLen(len(input)))
 	_, err := hex.Decode(rawBytes, []byte(input))
 	if err != nil {
 		fmt.Println(err)
 	}
 	return rawBytes
+}
+
+func BytesToHexString(input []byte) string {
+	output := make([]byte, hex.EncodedLen(len(input)))
+	hex.Encode(output, input)
+	return string(output)
 }
