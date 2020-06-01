76ff9a4caee8f679b5a2b310ca956b0d1fa14e48
Noah Snelson <noah.snelson@protonmail.com>
2020-05-31 21:23:27 -0700

    Continue work on challenge 13

--- set2/challenge13.go
+++ set2/challenge13.go
@@ -1,14 +1,22 @@
 package set2
 
 import (
+	"cryptopals/set1"
 	"fmt"
 	"strings"
 )
 
 func Challenge13() {
-	cookieString := "foo=bar&baz=qux&zap=zazzle"
-	parsedJson := ParseCookie(cookieString)
-	fmt.Println("SOLUTION 13:", parsedJson)
+	profileString := (ProfileFor("poop@gmail.com"))
+	fmt.Println(string(profileString), len(profileString))
+	roleIndex := strings.Index(profileString, "role=user")
+	fmt.Println(roleIndex, string(profileString[roleIndex:]))
+	profile := []byte(profileString)
+	key := []byte("YELLOW SUBMARINE")
+	encryptedCookie := set1.EncryptECB(PadPKCS(profile, GetPaddedLength(profile)), key)
+	adminCookie := MangleToAdmin(encryptedCookie, key, roleIndex)
+	fmt.Println("SOLUTION 13:", string(adminCookie), len(adminCookie))
+	fmt.Println(string(set1.DecryptECB(adminCookie, key)))
 }
 
 func ParseCookie(input string) map[string]string {
@@ -26,4 +34,23 @@ func ProfileFor(email string) string {
 	email = strings.ReplaceAll(email, "=", "")
 	cookie := string(append([]byte(email), []byte("&uid=10&role=user")...))
 	return cookie
+}
+
+func MangleToAdmin(encrypted, key []byte, roleIndex int) []byte {
+	roleIndex += 5 // go from index of 'r' in role to index of 'u' in user
+	targetSubstr := []byte("admin")
+	candidate := encrypted
+	fmt.Println("starting with,", string(set1.DecryptECB(candidate, key)))
+	for _, targetChar := range targetSubstr {
+		for byte := byte(0); byte < 255; byte++ {
+			candidate[roleIndex] = byte
+			decryptedCandidate := set1.DecryptECB(candidate, key)
+			if decryptedCandidate[roleIndex] == targetChar {
+				fmt.Printf("matched char %v at idx %v: %v\n", string(byte), roleIndex, string(set1.DecryptECB(candidate, key)))
+				roleIndex += 1
+				break
+			}
+		}
+	}
+	return candidate
 }
