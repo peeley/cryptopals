f0d1d57f5bbfdd42e9421c8462b8ed63cfeefc4a
Noah Snelson <noah.snelson@protonmail.com>
2020-10-05 23:12:52 -0700

    Modify solution 12 to actually use detected block size

--- set2/challenge12.go
+++ set2/challenge12.go
@@ -11,40 +11,41 @@ import (
 func Challenge12() {
 	fmt.Println("SOLUTION 12:")
 	blockSize := FindBlockSize()
-	fmt.Println("detecting a block size of: ", blockSize)
+	fmt.Println("  detecting a block size of ", blockSize)
 	revealed := ByteAtATimeDecrypt(blockSize)
-	fmt.Printf("cracked: '%v'\n", string(revealed))
+	fmt.Printf("  cracked: \n'%v'\n", string(revealed))
 }
 
 func FindBlockSize() int {
-	dummyInput := []byte{'A'}
+	dummyInput := []byte{}
 	initialLength := len(EncryptionOracle(dummyInput))
 	for {
 		dummyInput = append(dummyInput, 'A')
 		thisLength := len(EncryptionOracle(dummyInput))
-		if thisLength-initialLength >= 1 {
-			return thisLength - initialLength
+		lengthDiff := thisLength - initialLength
+		if lengthDiff >= 1 {
+			return lengthDiff
 		}
 	}
 }
 
-func EncryptionOracle(input []byte) []byte {
+func EncryptionOracle(userInput []byte) []byte {
 	rand.Seed(69) // create consistent but unknown key
 	key := RandomBytes(16)
 	rand.Seed(time.Now().UnixNano()) // reset for future keys to be random
 	unknown, _ := base64.StdEncoding.DecodeString("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK")
-	combined := append(input, unknown...)
+	combined := append(userInput, unknown...)
 	return PadAndEncryptECB(combined, key)
 }
 
 func ByteAtATimeDecrypt(blockSize int) []byte {
-	unknownLength := len(EncryptionOracle([]byte{}))
+	oracleLength := len(EncryptionOracle([]byte{}))
 	var cracked []byte
-	for idx := 0; idx < unknownLength; idx++ {
-		prefixLength := GetPaddedLength(cracked) - len(cracked) - 1
+	for length := 0; length < oracleLength; length++ {
+		prefixLength := (blockSize - (len(cracked) % blockSize)) - 1
 		prefix := make([]byte, prefixLength)
-		for idx, _ := range prefix {
-			prefix[idx] = 'A'
+		for prefixIdx := range prefix {
+			prefix[prefixIdx] = 'A'
 		}
 		shortOutput := EncryptionOracle(prefix)
 		testBlock := append(prefix, cracked...)
